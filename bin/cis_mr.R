4b807390c0c804d7bcd633e197c2ea94c53263fa
Guillermo Comesana Cimadevila <comesanaguillermo@gmail.com>
2026-06-09 18:41:49 +0100

    Finalise cis-MR run: need to make an if n_prot >= 1 at the end

--- bin/cis_mr.R
+++ bin/cis_mr.R
@@ -7,11 +7,13 @@
 # remotes::install_github("MRCIEU/TwoSampleMR")
 # if (!requireNamespace("remotes", quietly = TRUE)) install.packages("remotes")
 # remotes::install_github("MRCIEU/genetics.binaRies")
+# remotes::install_github("mrcieu/ieugwasr")
 
 suppressPackageStartupMessages({
   library(remotes)
   library(progress)
   library(TwoSampleMR)
+  library(ieugwasr)
   library(genetics.binaRies)
   library(data.table)
   library(arrow)
@@ -20,8 +22,8 @@ suppressPackageStartupMessages({
 args <- commandArgs(trailingOnly = TRUE)
 # args -> database (ukb-ppp) -> pheno1 -> out_dir
 
-if (length(args) < 6) {
-  stop("Usage: Rscript cis_mr.R <pQTL_dataset> <pqtl_dir> <pheno_id> <pheno_gwas> <ref_bfile> <out_dir>")
+if (length(args) < 5) {
+  stop("Usage: Rscript cis_mr.R <pQTL_dataset> <pqtl_dir> <pheno_id> <pheno_gwas> <ref_bfile>")
 }
 
 pqtl_dataset <- args[1]
@@ -29,8 +31,9 @@ pqtl_dir     <- args[2]
 pheno_id     <- args[3]
 pheno_gwas   <- args[4]
 ref_bfile    <- args[5]
-out_dir      <- args[6]
+# out_dir      <- args[6]
 
+out_dir <- "./results/cis-MR"
 
 # MR params
 # clump_kb, clump_r2, clump_p1
@@ -75,11 +78,17 @@ mr_function <- function(pqtl_dataset, pqtl_dir, pheno_id, pheno_gwas, ref_bfile,
       clear = FALSE
     )
     
+    # compile all res
+    all_results <- list()
+    
     for (i in proteins) {
-      pb$tick()
       protein <- tools::file_path_sans_ext(basename(i))
       print(paste0("[TRACKING] Processing ", protein))
       df <- arrow::read_parquet(i)
+      df <- as.data.table(df)
+      setorder(df, P)
+      df <- df[!duplicated(SNP)]
+      df <- as.data.frame(df)
       print(dim(df))
       
       # read exposure (i.e. pQTL)
@@ -127,8 +136,12 @@ mr_function <- function(pqtl_dataset, pqtl_dir, pheno_id, pheno_gwas, ref_bfile,
           plink_bin = genetics.binaRies::get_plink_binary(),
           bfile = ref_bfile
         )
+      } else {
+        print(paste0("No harmonised SNPs for ", protein))
+        pb$tick(tokens = list(protein = protein))
+        next
       }
-    
+      
       # ~~~~~~~~~~
       # run MR
       # ~~~~~~~~~~
@@ -143,37 +156,83 @@ mr_function <- function(pqtl_dataset, pqtl_dir, pheno_id, pheno_gwas, ref_bfile,
         )
         
         res.pleio <- mr_pleiotropy_test(dat.clump)
-        res.het   <- mr_heterogeneity(dat.clump, method_list = c("mr_ivw"))
+        res.het <- mr_heterogeneity(dat.clump, method_list = c("mr_ivw"))
         res.temp <- data.table::as.data.table(res.temp)
-        res.temp[, egger_intercept := res.pleio$egger_intercept[1]]
-        res.temp[, egger_intercept_pval := res.pleio$pval[1]]
-        res.temp[, Q := res.het$Q[1]]
-        res.temp[, Q_df := res.het$Q_df[1]]
-        res.temp[, Q_pval := res.het$Q_pval[1]]
         
       } else if (nrow(dat.clump) == 1) {
         res.temp <- mr(dat.clump, method_list = c("mr_wald_ratio"))
+        res.pleio <- data.table(egger_intercept = NA_real_, pval = NA_real_)
+        res.het <- data.table(Q = NA_real_, Q_df = NA_real_, Q_pval = NA_real_)
         res.temp <- data.table::as.data.table(res.temp)
-        res.temp[, egger_intercept := NA_real_]
-        res.temp[, egger_intercept_pval := NA_real_]
-        res.temp[, Q := NA_real_]
-        res.temp[, Q_df := NA_real_]
-        res.temp[, Q_pval := NA_real_]
         
       } else {
         print(paste0("Not enough instruments for ", protein))
+        pb$tick(tokens = list(protein = protein))
         next
       }
+      
+      res.temp <- dcast(res.temp, id.exposure + id.outcome ~ method, value.var = c("b", "se", "pval"))
+      setnames(res.temp, old = grep("Inverse variance weighted", names(res.temp), value = TRUE), new = gsub("Inverse variance weighted", "IVW", grep("Inverse variance weighted", names(res.temp), value = TRUE)))
+      setnames(res.temp, old = grep("MR Egger", names(res.temp), value = TRUE), new = gsub("MR Egger", "Egger", grep("MR Egger", names(res.temp), value = TRUE)))
+      setnames(res.temp, old = grep("Weighted median", names(res.temp), value = TRUE), new = gsub("Weighted median", "WME", grep("Weighted median", names(res.temp), value = TRUE)))
       res.temp[, protein := protein]
       res.temp[, outcome_trait := pheno_id]
       res.temp[, n_instruments := nrow(dat.clump)]
-      res.temp[, FDR_q := p.adjust(pval, method = "fdr")]
-      out_file <- file.path(out_dir, paste0(protein, "_", pheno_id, "_MR.tsv"))
-      fwrite(res.temp, out_file, sep = "\t")
-      print(paste0("Saved MR results: ", out_file))
+      res.temp[, egger_intercept := res.pleio$egger_intercept[1]]
+      res.temp[, egger_intercept_pval := res.pleio$pval[1]]
+      res.temp[, Q := res.het$Q[1]]
+      res.temp[, Q_df := res.het$Q_df[1]]
+      res.temp[, Q_pval := res.het$Q_pval[1]]
+      all_results[[protein]] <- res.temp
       pb$tick(tokens = list(protein = protein))
-    } 
+    }
     
+    all_results <- rbindlist(all_results, fill = TRUE)
+    if ("pval_IVW" %in% names(all_results)) {all_results[, IVW_FDR_q := p.adjust(pval_IVW, method = "fdr")]}
+    if ("pval_Egger" %in% names(all_results)) {all_results[, Egger_FDR_q := p.adjust(pval_Egger, method = "fdr")]}
+    if ("pval_WME" %in% names(all_results)) {all_results[, WME_FDR_q := p.adjust(pval_WME, method = "fdr")]}
+    
+    # reformat for shiny app / dashboard
+    setnames(all_results,
+             old = c(
+               "b_IVW", "se_IVW", "pval_IVW",
+               "b_Egger", "se_Egger", "pval_Egger",
+               "b_WME", "se_WME", "pval_WME"
+             ),
+             new = c(
+               "IVW_beta", "IVW_se", "IVW_pval",
+               "Egger_beta", "Egger_se", "Egger_pval",
+               "WME_beta", "WME_se", "WME_pval"
+             ),
+             skip_absent = TRUE
+    )
+    
+    all_results <- all_results[, .(
+      protein,
+      outcome_trait,
+      n_instruments,
+      IVW_beta,
+      IVW_se,
+      IVW_pval,
+      IVW_FDR_q,
+      Egger_beta,
+      Egger_se,
+      Egger_pval,
+      Egger_FDR_q,
+      egger_intercept,
+      egger_intercept_pval,
+      WME_beta,
+      WME_se,
+      WME_pval,
+      WME_FDR_q,
+      Q,
+      Q_df,
+      Q_pval
+    )]
+    
+    out_file <- file.path(out_dir, paste0(pqtl_dataset, "_", pheno_id, "_all_MR.tsv"))
+    fwrite(all_results, out_file, sep = "\t")
+    print(paste0("Saved all MR results: ", out_file))
   }
 }
 
@@ -185,6 +244,10 @@ mr_function(
   ref_bfile    = ref_bfile,
   out_dir      = out_dir
 )
+
+# rename output colnames 
+# make ifs for if n proteins > 1 perform FDR if not !=
+
 
 # first do an if pQTL_dataset == ukb_ppp
 # read_parquet
