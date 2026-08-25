3557eeba4b908d884614f5d844c2c1942534d6e3
Guillermo Comesana Cimadevila <comesanaguillermo@gmail.com>
2026-08-25 21:45:43 +0100

    udpate pwoco wrapper

--- bin/pwcoco_wrapper.py
+++ bin/pwcoco_wrapper.py
@@ -3,11 +3,11 @@ from pathlib import Path
 from drugmr.pwcoco import PWCoCo
 from drugmr.utils import grab_cis_mr_hits
 from drugmr.paths import pwcoco_out, pwcoco_raw_dir, pwcoco_raw_prefix, mr_out
-import polars as pl 
+import polars as pl
 
 """ Workflow wrapper (pre-Snakemake) for PWCoCo """
 
-def run_pwcoco(ref_bile: str, n_cases: int, n_controls: int, pqtl_dataset: str, pheno_id: str, cochran_q_thresh: float = 0.05, causal_thresh: float = 0.05):
+def run_pwcoco(ref_bfile: str, n_cases: int, n_controls: int, pqtl_dataset: str, pheno_id: str, cochran_q_thresh: float = 0.05, causal_thresh: float = 0.05):
 
     """
     PWCoCo wrapper for workflow
@@ -23,48 +23,48 @@ def run_pwcoco(ref_bile: str, n_cases: int, n_controls: int, pqtl_dataset: str, 
     )
 
     for target in targets:
-        cis_regions = f"./dat/cis_regions/{pqtl_dataset}/{target}"
-        cis_regions = Path(cis_regions)
-        for file in cis_regions.glob("*.parquet"):
-            gwas = "gwas.parquet"
-            pqtl = "pqtl.parquet"
+        cis_regions = Path(f"./dat/cis_regions/{pqtl_dataset}/{target}")
+        gwas = pl.read_parquet(cis_regions / "gwas.parquet")
+        pqtl = pl.read_parquet(cis_regions / "pqtl.parquet")
 
-            # harmonise sumstats (pre-PWCoCo)
-            # GWAS
-            gwas = pwcoco.harmonise_sumstats(
-                df=gwas,
-                snp_col="SNP",
-                a1_col="A1",
-                a2_col="A2",
-                maf_col="FRQ",
-                beta_col="BETA",
-                se_col="SE",
-                p_col="P",
-                n_col="N"
-            )
+        # harmonise sumstats (pre-PWCoCo)
+        # GWAS
+        gwas = pwcoco.harmonise_sumstats(
+            df=gwas,
+            snp_col="SNP",
+            a1_col="A1",
+            a2_col="A2",
+            maf_col="FRQ",
+            beta_col="BETA",
+            se_col="SE",
+            p_col="P",
+            n_col="N"
+        )
 
-            # pQTL
-            pqtl = pwcoco.harmonise_sumstats(
-                df=pqtl,
-                snp_col="SNP",
-                a1_col="A1",
-                a2_col="A2",
-                maf_col="FRQ",
-                beta_col="BETA",
-                se_col="SE",
-                p_col="P",
-                n_col="N"
-            )
+        # pQTL
+        pqtl = pwcoco.harmonise_sumstats(
+            df=pqtl,
+            snp_col="SNP",
+            a1_col="A1",
+            a2_col="A2",
+            maf_col="FRQ",
+            beta_col="BETA",
+            se_col="SE",
+            p_col="P",
+            n_col="N"
+        )
 
-            # run PWCoCo
-            pwcoco.pwcoco(
-                ref_bfile=ref_bile,
-                sumstats_1=pqtl,
-                sumstats_2=gwas,
-                threads=8,
-                n_2=(n_cases + n_controls),
-                n2_case=n_cases,
-                # out_dir=
-                # n_1
-            )
-        
+        out_prefix = pwcoco_raw_prefix(pqtl_dataset, pheno_id, target)
+        out_prefix.parent.mkdir(parents=True, exist_ok=True)
+
+        # run PWCoCo
+        pwcoco.pwcoco(
+            ref_bfile=ref_bfile,
+            sumstats_1=pqtl,
+            sumstats_2=gwas,
+            threads=8,
+            n_1=int(pqtl["n"][0]),
+            n_2=(n_cases + n_controls),
+            n2_case=n_cases,
+            out_dir=str(out_prefix),
+        )
